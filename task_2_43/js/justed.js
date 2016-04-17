f8742a674bf480788f9c1421b06e7f1474a39b95
zd <zhendong9860@163.com>
2016-04-17 23:44:56 +0800

    task_2_43 add css and imgs to finish the js

---
+++ task_2_43/js/justed.js
@@ -0,0 +1,17 @@
+/**
+ * Created by zd98 on 2016/4/17.
+ */
+var a = function(){
+
+  return {
+    create:function(dom,images,adjust,num,config){
+      //container width height 根据用户自己设定
+      //images 限制MAX =6;
+      var container = document.querySelector(dom);
+      container.classList.add('container');
+      var options = {};
+
+      
+    }
+  }
+}();
