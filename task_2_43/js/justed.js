a94139feca89186ea0deae36b248dfa10dca64d7
zd <zhendong9860@163.com>
2016-04-19 22:01:44 +0800

    finish the puzzle

--- task_2_43/js/justed.js
+++ task_2_43/js/justed.js
@@ -1,17 +1,21 @@
-/**
- * Created by zd98 on 2016/4/17.
- */
-var a = function(){
-
-  return {
-    create:function(dom,images,adjust,num,config){
-      //container width height 根据用户自己设定
-      //images 限制MAX =6;
-      var container = document.querySelector(dom);
-      container.classList.add('container');
-      var options = {};
-
-      
+(function(window){
+  'use strict';
+  function ZdAlbum(){
+    this.LAYOUT = {
+      PUZZLE: 1,    // 拼图布局
+      WATERFALL: 2, // 瀑布布局
+      BARREL: 3     // 木桶布局
     }
   }
-}();
+
+
+  ZdAlbum.prototype.setImage = function (images,options){
+
+  };
+  ZdAlbum.prototype.addImage = function (image,config){
+
+  };
+
+
+
+})(window);
